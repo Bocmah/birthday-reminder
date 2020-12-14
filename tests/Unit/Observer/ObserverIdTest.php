9febeee849cfedaef0c2a82d86b77d85035ea97a
Artem Trushkin <at@kodix.ru>
2020-12-14 22:50:47 +0300

    Improve ObserverIdTest

--- tests/Unit/Observer/ObserverIdTest.php
+++ tests/Unit/Observer/ObserverIdTest.php
@@ -9,10 +9,26 @@ use Vkbd\Observer\ObserverId;
 
 final class ObserverIdTest extends TestCase
 {
-    public function test_you_can_not_create_observer_id_providing_not_positive_number(): void
+    /**
+     * @dataProvider idsProvider
+     *
+     * @param int $invalidId
+     */
+    public function test_you_can_not_create_observer_id_providing_not_positive_number(int $invalidId): void
     {
         $this->expectExceptionMessage('Observer id must be positive');
 
-        new ObserverId(-1);
+        new ObserverId($invalidId);
+    }
+
+    /**
+     * @return iterable<int[]>
+     */
+    public function idsProvider(): iterable
+    {
+        return [
+            [-1],
+            [0]
+        ];
     }
 }
