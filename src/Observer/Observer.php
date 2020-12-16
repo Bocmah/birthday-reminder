7f59ae37cb446d89b0ea08db7001e75d8e236b52
Artem Trushkin <at@kodix.ru>
2020-12-17 00:01:33 +0300

    Create observer entity

---
+++ src/Observer/Observer.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vkbd\Observer;
+
+use Vkbd\Person\FullName;
+use Vkbd\Vk\NumericVkId;
+
+final class Observer
+{
+    private ObserverId $id;
+    private NumericVkId $vkId;
+    private FullName $fullName;
+    private bool $shouldAlwaysBeNotified;
+
+    public function __construct(ObserverId $id, NumericVkId $vkId, FullName $fullName, bool $shouldAlwaysBeNotified = true)
+    {
+        $this->id = $id;
+        $this->vkId = $vkId;
+        $this->fullName = $fullName;
+        $this->shouldAlwaysBeNotified = $shouldAlwaysBeNotified;
+    }
+
+    public function id(): ObserverId
+    {
+        return $this->id;
+    }
+
+    public function vkId(): NumericVkId
+    {
+        return $this->vkId;
+    }
+
+    public function fullName(): FullName
+    {
+        return $this->fullName;
+    }
+
+    public function shouldAlwaysBeNotified(): bool
+    {
+        return $this->shouldAlwaysBeNotified;
+    }
+}
