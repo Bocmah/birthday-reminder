7d2e9c6889c1f6dee726463689c8e35b97ddbb44
Artem Trushkin <at@kodix.ru>
2020-12-17 23:14:23 +0300

    Create EventTest

---
+++ tests/Unit/Vk/EventTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Vk;
+
+use PHPUnit\Framework\TestCase;
+use Vkbd\Vk\Event;
+
+final class EventTest extends TestCase
+{
+    public function test_it_rejects_wrong_values(): void
+    {
+        new Event(Event::CONFIRMATION);
+        new Event(Event::NEW_MESSAGE);
+
+        $this->expectExceptionMessage('Unknown event type');
+
+        new Event('foo');
+    }
+}
