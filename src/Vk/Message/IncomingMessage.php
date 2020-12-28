c38001dc92124457f40bb43358e1227cf9a0a174
Artem Trushkin <at@kodix.ru>
2020-12-28 23:15:49 +0300

    Create IncomingMessage

---
+++ src/Vk/Message/IncomingMessage.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vkbd\Vk\Message;
+
+use Vkbd\Vk\NumericVkId;
+
+final class IncomingMessage
+{
+    private NumericVkId $from;
+    private string $text;
+
+    public function __construct(NumericVkId $from, string $text)
+    {
+        $this->from = $from;
+        $this->text = $text;
+    }
+
+    public function from(): NumericVkId
+    {
+        return $this->from;
+    }
+
+    public function text(): string
+    {
+        return $this->text;
+    }
+}
