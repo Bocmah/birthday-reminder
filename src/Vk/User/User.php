e78d357d45f94b7800f92dda6461a5d85f772e6b
Artem Trushkin <at@kodix.ru>
2021-01-02 22:09:37 +0300

    Create VK User

---
+++ src/Vk/User/User.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vkbd\Vk\User;
+
+use Vkbd\Person\FullName;
+
+final class User
+{
+    private NumericVkId $id;
+    private FullName $fullName;
+
+    public function __construct(NumericVkId $id, FullName $fullName)
+    {
+        $this->id = $id;
+        $this->fullName = $fullName;
+    }
+
+    public function id(): NumericVkId
+    {
+        return $this->id;
+    }
+
+    public function fullName(): FullName
+    {
+        return $this->fullName;
+    }
+}
