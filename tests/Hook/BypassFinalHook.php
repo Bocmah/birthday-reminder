6cd7c8d5f946aa05afa38830bf7b73f22fecfe1a
Artem Trushkin <at@kodix.ru>
2021-01-02 17:32:51 +0300

    Create BypassFinalHook for PHPUnit

---
+++ tests/Hook/BypassFinalHook.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @noinspection PhpUnused */
+
+declare(strict_types=1);
+
+namespace Tests\Hook;
+
+use DG\BypassFinals;
+use PHPUnit\Runner\BeforeTestHook;
+
+final class BypassFinalHook implements BeforeTestHook
+{
+    public function executeBeforeTest(string $test): void
+    {
+        BypassFinals::enable();
+    }
+}
