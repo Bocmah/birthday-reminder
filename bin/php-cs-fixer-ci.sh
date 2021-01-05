574f05d616d1914f14da21e1e3f7ae15032caa9c
Artem Trushkin <at@kodix.ru>
2021-01-06 00:14:29 +0300

    Fix php-cs-fixer-ci.sh

--- bin/php-cs-fixer-ci.sh
+++ bin/php-cs-fixer-ci.sh
@@ -3,15 +3,12 @@
 IFS='
 '
 
-COMMIT_RANGE="HEAD~..HEAD"
-CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB "${COMMIT_RANGE}")
+CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB HEAD)
 
 if ! echo "${CHANGED_FILES}" | grep -qE "^(\\.php_cs(\\.dist)?|composer\\.lock)$"; then
     EXTRA_ARGS=$(printf -- '--path-mode=intersection\n--\n%s' "${CHANGED_FILES}")
 else
     EXTRA_ARGS='';
 fi
-
-cd ..
 
 composer php-cs-fixer -- --config=.php_cs.dist -v --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
