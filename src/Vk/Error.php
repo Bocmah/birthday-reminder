1c0e1dfdee9949e0e2d1d13058b47bb2e3ad6844
Artem Trushkin <at@kodix.ru>
2021-01-12 21:41:12 +0300

    Create Vk\Error

---
+++ src/Vk/Error.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vkbd\Vk;
+
+use InvalidArgumentException;
+
+final class Error
+{
+    public const INVALID_USER_ID = 113;
+
+    private int $code;
+
+    public function __construct(int $code)
+    {
+        if ($code !== self::INVALID_USER_ID) {
+            throw new InvalidArgumentException('Unknown error code');
+        }
+
+        $this->code = $code;
+    }
+
+    public function is(int $code): bool
+    {
+        return $this->code === $code;
+    }
+}
