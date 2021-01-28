7e723037e31f2c0e6501cbd2985f2d539db25c8d
Artem Trushkin <at@kodix.ru>
2021-01-28 22:57:34 +0300

    Create TranslatableException::withTranslatableMessage()

--- src/Translation/TranslatableException.php
+++ src/Translation/TranslatableException.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Vkbd\Translation;
 
 use RuntimeException;
+use Symfony\Component\Translation\TranslatableMessage;
 use Symfony\Contracts\Translation\TranslatableInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -12,9 +13,9 @@ final class TranslatableException extends RuntimeException implements Translatab
 {
     private TranslatableInterface $translatable;
 
-    public function __construct(TranslatableInterface $translatable, string $message = '')
+    public function __construct(TranslatableInterface $translatable, string $exceptionMessage = '')
     {
-        parent::__construct($message);
+        parent::__construct($exceptionMessage);
 
         $this->translatable = $translatable;
     }
@@ -22,5 +23,13 @@ final class TranslatableException extends RuntimeException implements Translatab
     public function trans(TranslatorInterface $translator, ?string $locale = null): string
     {
         return $this->translatable->trans($translator, $locale);
+    }
+
+    public static function withTranslatableMessage(string $message, array $parameters = [], string $exceptionMessage = ''): self
+    {
+        return new self(
+            new TranslatableMessage($message, $parameters),
+            $exceptionMessage
+        );
     }
 }
