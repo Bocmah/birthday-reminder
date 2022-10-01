e3d707869e6eb83541c41dc4583cacbd38caf33b
Bocmah <atrushkin1@gmail.com>
2022-10-01 23:24:20 +0300

    Finish MessageReceiver

--- config/bundles.php
+++ config/bundles.php
@@ -6,10 +6,12 @@ use Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
 
 return [
     FrameworkBundle::class => ['all' => true],
     BaldinofRoadRunnerBundle::class => ['all' => true],
     DoctrineBundle::class => ['all' => true],
     DoctrineMigrationsBundle::class => ['all' => true],
+    MonologBundle::class => ['all' => true],
 ];
