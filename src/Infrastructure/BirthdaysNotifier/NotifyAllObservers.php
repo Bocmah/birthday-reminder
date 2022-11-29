f1ca2d52f6d7c81d18e44a4fe7bf40da2af44acf
Bocmah <atrushkin1@gmail.com>
2022-11-29 23:48:40 +0300

    Add notify all observers symfony command

---
+++ src/Infrastructure/BirthdaysNotifier/NotifyAllObservers.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BirthdayReminder\Infrastructure\BirthdaysNotifier;
+
+use BirthdayReminder\Application\BirthdaysNotifierService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'observers:notify', description: 'Notify all observers about upcoming birthdays.')]
+final class NotifyAllObservers extends Command
+{
+    private function __construct(private readonly BirthdaysNotifierService $notifierService)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->notifierService->notifyObservers();
+
+        return Command::SUCCESS;
+    }
+}
